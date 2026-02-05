d41419a6046660a6994a18c3581ca31f2ac5e001
unpayedme <jv.sefuesca@gmail.com>
2026-02-05 15:08:36 +0800

    added blog slug

--- app/(guess)/blog/page.tsx
+++ app/(guess)/blog/page.tsx
@@ -1,8 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Blog() {  
   return (
-    <div>
+    <div className="container m-auto p-6">
       <h1>Blog Page</h1>
       <p>This is the Blog page.</p>
+      <div className="mt-4">
+        <Link href="./blog/my-first-blog">
+          <Button>
+            Visit My First Blog!
+          </Button>
+        </Link>
+        
+      </div>
     </div>
   );
 }
