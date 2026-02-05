48f43b9d63a7bec72cb5f60fdc926260ff1471ec
unpayedme <jv.sefuesca@gmail.com>
2026-02-05 14:35:08 +0800

    added header added darkmode

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,17 +1,31 @@
 import { Button } from "@/components/ui/button";
-
-export function Header(){
-    return(
-        <div className="border container m-auto"> 
-            <h1>PORTFOLIO</h1>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Blogs</li>
-            </ul>
-            <div>
-                <Button />
+import Link from "next/link";
+import { ModeToggle } from "@/components/common/themetoggle";
+export function Header() {
+    return (
+        <div className="border-b border-gray-700 p-6">
+            <div className="container m-auto" >
+                <div className="flex justify-around items-center">
+                    <h1 className="font-bold">PORTFOLIO</h1>
+                    <ul className="flex gap-6">
+                        <li>
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link href="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link href="/projects">Projects</Link>
+                        </li>
+                        <li>
+                            <Link href="/blog">Blogs</Link>
+                        </li>
+                    </ul>
+                    <div className="flex gap-4">
+                        <Button>Contact</Button>
+                        <ModeToggle />
+                    </div>
+                </div>
             </div>
         </div>
     );
