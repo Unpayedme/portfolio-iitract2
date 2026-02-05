4a5632503487b3530544c4b0f4126ca0a5ae1e19
unpayedme <jv.sefuesca@gmail.com>
2026-02-05 13:01:50 +0800

    add some folder

---
+++ app/(guess)/(index)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {  
+  return (
+    <div>
+      <h1>Guess Page</h1>
+      <p>This is the guess page.</p>
+    </div>
+  );
+}
---
+++ app/(guess)/projects/page.tsx
@@ -0,0 +1,8 @@
+export default function Project() {  
+  return (
+    <div>
+      <h1>Project Page</h1>
+      <p>This is the Project page.</p>
+    </div>
+  );
+}
