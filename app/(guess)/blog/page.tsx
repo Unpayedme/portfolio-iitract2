61475bb0eca8d61e259193d6943c87daa16f0bda
unpayedme <jv.sefuesca@gmail.com>
2026-02-10 14:08:33 +0800

    added constants blog.ts and change the layout of the blog

--- app/(guess)/blog/page.tsx
+++ app/(guess)/blog/page.tsx
@@ -1,19 +1,39 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { BLOG_CATEGORIES, BLOG_POSTS } from "@/constants/blob";
 
-export default function Blog() {  
+export default function Blog() {
   return (
-    <div className="container m-auto p-6">
-      <h1>Blog Page</h1>
-      <p>This is the Blog page.</p>
-      <div className="mt-4">
-        <Link href="./blog/my-first-blog">
-          <Button>
-            Visit My First Blog!
-          </Button>
+    <div className="container m-auto py-6">
+      <h1 className="text-3xl font-bold">Blog</h1>
+
+      <div className="my-6 flex gap-2">
+        <Link href="/blog">
+          <Button variant="outline">All Posts</Button>
         </Link>
-        
+
+
+        {
+          BLOG_CATEGORIES.map((category,index) => (
+            <Link key={index} href={`blog/category/${category.slug}`}>
+                <Button variant="outline">{category.name}</Button>
+            </Link>
+          ))
+        }
       </div>
+
+      <div className="flex flex-col gap-4">
+        {BLOG_POSTS.map(category => (
+          <Link key={category.id} href={`blog/category/${category.slug}`}>
+            <div className="border rounded-lg hover:muted/50 p-4 gap-4">
+              <h1 className="font-bold">{category.title}</h1>
+              <p className="text-muted-foreground">{category.overview}</p>
+              <p className="text-muted-foreground">{category.date} • {category.author}</p>
+            </div>
+          </Link>
+        ))}
+      </div>
+
     </div>
   );
 }
